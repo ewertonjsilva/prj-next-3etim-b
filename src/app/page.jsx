a426c8c71e8f2de24f25d7176d0aac7e260cffe5
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-06-14 11:07:10 -0300

    aula 14-06

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,16 @@
 import styles from "./page.module.css";
-import Link from "next/link";
+import Link from "next/link"; 
+import Image from "next/image"; 
+
+import logo from '../../public/lanche.jpeg'
 
 export default function Home() {
   return (
     <div className="container">
       <h1>Home</h1>
+      <div>
+        <Image src='/home/lanche.jpeg' width={100} height={100}/>
+      </div>
       <Link  href={"/carrinho"}>Carrinho</Link>
     </div>
   );
